b43643eda9325f61fe17414545b4497686e7177d
banieves <123413453+banieves@users.noreply.github.com>
2023-02-06 14:41:23 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,55 @@
-﻿namespace Homework1;
+﻿namespace Homework2;
 class Program
 {
     static void Main(string[] args)
-    {
-        double X = 2.5;
-        double Y = 3.3;
-        double Z;
-        Z=4*(X*X)+3*Y;
-        
-        Console.Write("X = " + X + ",");
-        Console.WriteLine(" Y = " + Y);
-        Console.WriteLine("The value of Z is:" + Z);
-
-
-      
-
+    { 
+        Console.WriteLine("Please input a letter grade: ");
+        string letter = Console.ReadLine(); // "A"
+        // int point_string = Convert.ToInt16(point);
+        if(letter=="A"){
+            Console.WriteLine("GPA Point: 4");
+        }
+        else if (letter=="B"){
+            Console.WriteLine("GPA Point: 3");
+        }
+        else if (letter=="C"){
+            Console.WriteLine("GPA Point: 2");
+        }
+        else if (letter=="D"){
+            Console.WriteLine("GPA Point: 1");
+        }
+        else if (letter=="F"){
+            Console.WriteLine("GPA Point: 0");
+        }
+        else {
+            Console.WriteLine("Wrong Letter Grade!");
+        }
+        Console.WriteLine("Please input the first num: ");
+        string num1 = Console.ReadLine();
+        int num1_int = Convert.ToInt16(num1);
+        Console.WriteLine("Please input the second num: ");
+        string num2 = Console.ReadLine();
+        int num2_int = Convert.ToInt16(num2);
+        Console.WriteLine("Please input the third num: ");
+        string num3 = Console.ReadLine();
+        int num3_int = Convert.ToInt16(num3);
+         if(num1_int<num2_int){
+            if(num1_int<num3_int){
+                Console.WriteLine("The smallest number is: {num1_int}");
+            }
+            else{//n1<=n3
+                Console.WriteLine("The smallest number is: {num3_int}");
+            }
+        }
+        else{ //n2>=n1
+            if(num2_int<num3_int){
+                Console.WriteLine("The smalles number is: {num2_int}");
+            }
+            else{//n3>=n2
+                Console.WriteLine("The smallest number is: {num3_int}");
+            }
+        }
         
     }
+
 }
