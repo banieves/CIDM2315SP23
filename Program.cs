d6220d2af940ce506d17b4f56c13cca1f5fe6aaa
banieves <123413453+banieves@users.noreply.github.com>
2023-02-24 11:37:39 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double X = 2.5;
+        double Y = 3.3;
+        double Z;
+        Z=4*(X*X)+3*Y;
+        
+        Console.Write("X = " + X + ",");
+        Console.WriteLine(" Y = " + Y);
+        Console.WriteLine("The value of Z is:" + Z);
+
+
+      
+
+        
+    }
+}
